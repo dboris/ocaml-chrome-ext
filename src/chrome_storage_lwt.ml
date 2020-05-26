568cb15e76569a65b1acd0ea3f2db02b84430416
Boris D <boris.dob@gmail.com>
2020-05-25 20:42:25 -0700

    Refactor.

--- src/chrome_storage_lwt.ml
+++ src/chrome_storage_lwt.ml
@@ -1,29 +1,29 @@
 open Core_types
 
-module type T = sig
-    val get_key : Chrome_storage.key -> Ojs.t option Lwt.t
-    val get_keys : Chrome_storage.key list -> Ojs.t Dict.t Lwt.t
+module type STORAGE_LWT = sig
+    val get_key : string -> Ojs.t option Lwt.t
+    val get_keys : string list -> Ojs.t Dict.t Lwt.t
     val get_all : unit -> Ojs.t Dict.t Lwt.t
     val get_with_defaults : Ojs.t Dict.t -> Ojs.t Dict.t Lwt.t
     val set_key : string -> Ojs.t -> unit Lwt.t
     val set_keys : Ojs.t Dict.t -> unit Lwt.t
-    val remove_key : Chrome_storage.key -> unit Lwt.t
-    val remove_keys : Chrome_storage.key list -> unit Lwt.t
+    val remove_key : string -> unit Lwt.t
+    val remove_keys : string list -> unit Lwt.t
     val clear : unit -> unit Lwt.t
 end
 
-module type S = sig
-    val get_key : Chrome_storage.key -> callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
-    val get_keys : Chrome_storage.key list -> callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
+module type STORAGE = sig
+    val get_key : string -> callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
+    val get_keys : string list -> callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
     val get_all : callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
     val get_with_defaults : Ojs.t Dict.t -> callback:(Ojs.t Dict.t callback_arg -> unit) -> unit
     val set : Ojs.t Dict.t -> ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
-    val remove_key : Chrome_storage.key -> ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
-    val remove_keys : Chrome_storage.key list -> ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
+    val remove_key : string -> ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
+    val remove_keys : string list -> ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
     val clear : ?callback:(Ojs.t option callback_arg -> unit) -> unit -> unit
 end
 
-module Make_lwt_storage (S : S) : T = struct
+module Make_lwt_storage (S : STORAGE) : STORAGE_LWT = struct
     let get_key key : Ojs.t option Lwt.t =
         let%lwt result = wrap_required_callback (S.get_key key) in
         Lwt.return (
--- src/chrome_storage_lwt.mli
+++ src/chrome_storage_lwt.mli
@@ -1,16 +1,16 @@
 open Core_types
 
-module type T = sig
-    val get_key : Chrome_storage.key -> Ojs.t option Lwt.t
-    val get_keys : Chrome_storage.key list -> Ojs.t Dict.t Lwt.t
+module type STORAGE_LWT = sig
+    val get_key : string -> Ojs.t option Lwt.t
+    val get_keys : string list -> Ojs.t Dict.t Lwt.t
     val get_all : unit -> Ojs.t Dict.t Lwt.t
     val get_with_defaults : Ojs.t Dict.t -> Ojs.t Dict.t Lwt.t
     val set_key : string -> Ojs.t -> unit Lwt.t
     val set_keys : Ojs.t Dict.t -> unit Lwt.t
-    val remove_key : Chrome_storage.key -> unit Lwt.t
-    val remove_keys : Chrome_storage.key list -> unit Lwt.t
+    val remove_key : string -> unit Lwt.t
+    val remove_keys : string list -> unit Lwt.t
     val clear : unit -> unit Lwt.t
 end
 
-module Local : T
-module Sync : T
+module Local : STORAGE_LWT
+module Sync : STORAGE_LWT
