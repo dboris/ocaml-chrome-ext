5be8f9f750884b1f7bff343bcd2ce7e8ec6c0d0c
Boris D <boris.dob@gmail.com>
2020-07-07 20:04:41 -0700

    Improve lwt intf.

--- src/chrome_web_navigation_lwt.ml
+++ src/chrome_web_navigation_lwt.ml
@@ -1,8 +1,11 @@
 open Lwt.Infix
 open Core_types
 
-let get_frame opts : Chrome_web_navigation.frame_details Lwt.t =
+let get_frame ~tab_id ~frame_id : Chrome_web_navigation.frame_details Lwt.t =
+    let opts =
+      Chrome_web_navigation.get_frame_opts ~tabId:tab_id ~frameId:frame_id in
     wrap_required_callback (Chrome_web_navigation.get_frame opts)
 
-let get_all_frames opts : Chrome_web_navigation.frame_info list Lwt.t =
+let get_all_frames tab_id : Chrome_web_navigation.frame_info list Lwt.t =
+    let opts = Chrome_web_navigation.get_all_frames_opts tab_id in
     wrap_required_callback (Chrome_web_navigation.get_all_frames opts)
